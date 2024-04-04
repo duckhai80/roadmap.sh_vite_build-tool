aa64be411395cd443262bbd7d6f775e257f72734
Truong Duc Khai <duckhai80@gmail.com>
2024-04-04 23:10:33 +0700

    Using env variables and deployments

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  console.log(import.meta.env);
+  console.log(import.meta);
+  console.log(`Server is running on ${import.meta.env.MODE}`);
+
+  if (import.meta.env.DEV) {
+    console.log("This is Dev server!");
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="bg-black text-white text-5xl">Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
