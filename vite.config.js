9b0bd3b2988914dd910079b0cb372fa4d16c1c34
Truong Duc Khai <duckhai80@gmail.com>
2024-04-07 13:40:51 +0700

    Wrap up vite course

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/roadmap.sh_vite_build-tool/",
+  clearScreen: false,
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
 });
